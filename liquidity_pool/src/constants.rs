6198f9eafc8002a4ae55db09a4e447dced0c5354
Joshua Blew <joshua@normalfinance.io>
2025-05-08 10:20:59 -0500

    feat: add useful constants

--- liquidity_pool/src/constants.rs
+++ liquidity_pool/src/constants.rs
@@ -1,1 +1,6 @@
 pub(crate) const FEE_MULTIPLIER: u128 = 10_000;
+
+pub(crate) const PRICE_PRECISION: u128 = 1_000_000; //expo = -6;
+pub(crate) const PRICE_PRECISION_I128: i128 = PRICE_PRECISION as i128;
+pub(crate) const PRICE_PRECISION_U64: u64 = 1_000_000; //expo = -6;
+pub(crate) const PRICE_PRECISION_I64: i64 = 1_000_000; //expo = -6;
