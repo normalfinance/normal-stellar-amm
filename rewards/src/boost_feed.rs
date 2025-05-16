88f5deb039a0cda3894f628da1095d16712fd561
Joshua Blew <joshua@normalfinance.io>
2025-05-16 18:47:45 -0400

    refactor: organize contracts into own folder

--- rewards/src/boost_feed.rs
+++
@@ -1,6 +0,0 @@
-mod boost_feed {
-    soroban_sdk::contractimport!(
-        file = "../target/wasm32v1-none/release/soroban_locker_feed_contract.wasm"
-    );
-}
-pub use boost_feed::Client as RewardBoostFeedClient;
