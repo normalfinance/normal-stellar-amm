1a96f8f9f3a0fa9614af6e7e8dbdaa6dc671a24e
Joshua Blew <joshua@normalfinance.io>
2025-06-05 19:12:51 -0500

    feat: add log10 to helpers

--- contracts/utils/src/helpers.rs
+++ contracts/utils/src/helpers.rs
@@ -1,3 +1,8 @@
+#[cfg(test)]
+pub fn log10(n: u128) -> u128 {
+    if n < 10 { 0 } else if n == 10 { 1 } else { log10(n / 10) + 1 }
+}
+
 pub fn log10_iter(n: u128) -> u128 {
     let mut result = 0;
     let mut n_copy = n;
