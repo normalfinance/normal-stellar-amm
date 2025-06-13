7f22bf1d01ae525ddfd982bbb1f389ce11a5c213
Joshua Blew <joshua@normalfinance.io>
2025-06-13 11:32:17 -0500

    refactor: token utils

--- modules/utils/src/token.rs
+++ modules/utils/src/token.rs
@@ -1,10 +1,20 @@
-use soroban_sdk::token::{Client as SorobanTokenClient, TokenClient};
-use soroban_sdk::{Address, Env, Vec};
+use soroban_sdk::token::{ Client as SorobanTokenClient, TokenClient };
+use soroban_sdk::{ panic_with_error, Address, Env, Vec };
 
-pub fn validate_tokens_contracts(e: &Env, tokens: &Vec<Address>) {
+use crate::errors::validation_errors::ValidationError;
+
+pub fn validate_token_contract(e: &Env, token: &Address) {
     // call token contract to check if token exists & it's alive
+    let result = SorobanTokenClient::new(e, &token).try_balance(&e.current_contract_address());
+
+    if result.is_err() {
+        panic_with_error!(e, ValidationError::InvalidToken);
+    }
+}
+
+pub fn validate_token_contracts(e: &Env, tokens: &Vec<Address>) {
     for token in tokens.iter() {
-        SorobanTokenClient::new(e, &token).balance(&e.current_contract_address());
+        validate_token_contract(e, &token);
     }
 }
 
@@ -18,7 +28,7 @@ pub fn transfer_token_from(
     spender: &Address,
     from: &Address,
     to: &Address,
-    amount: &i128,
+    amount: &i128
 ) {
     SorobanTokenClient::new(e, token).transfer_from(spender, from, to, amount);
 }
