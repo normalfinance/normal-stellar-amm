5c31c3ffb4bf6c94c8443600f474996948c81022
Joshua Blew <joshua@normalfinance.io>
2025-09-02 14:13:39 -0500

    fix(scripts): enable clawback

--- scripts/sac/enable_clawback.sh
+++ scripts/sac/enable_clawback.sh
@@ -4,22 +4,21 @@ set -e
 # Usage
 usage() {
     echo "Usage:"
-    echo "  $0 <issuer> <network> <symbol>"
+    echo "  $0 <issuer> <network>"
     echo ""
     echo "Example:"
-    echo "  $0 admin testnet nSOL"
+    echo "  $0 admin testnet"
     exit 1
 }
 
 # Validate args
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 2 ]; then
     usage
 fi
 
 # Parse arguments
 ISSUER=$1
 NETWORK=$2
-SYMBOL=$3
 
 # Load env vars dynamically
 REPO_ROOT="$(git rev-parse --show-toplevel)"
@@ -27,15 +26,6 @@ source "$REPO_ROOT/scripts/load-env.sh" "$NETWORK"
 
 # Get admin address
 ISSUER_ADDRESS=$(soroban keys address "$ISSUER")
-
-# Issue an asset by creating a trustline
-# stellar tx new set-trustline-flags \
-#     --source-account "GCDPXHV4QZPDRCP65CKDAA4JBBNMDO5LTO3TCHC6HXGZV54HN63K3KY5" \
-#     --network "testnet" \
-#     --trustor "CAGD5SCICK23F65YUUGCRWSYGAQM4VPY36LG5WG2KQTMYCBOYD5WGO4W" \
-#     --asset "nETH:GCDPXHV4QZPDRCP65CKDAA4JBBNMDO5LTO3TCHC6HXGZV54HN63K3KY5" \
-#     --set-trustline-clawback-enabled
-
 
 stellar tx new set-options \
     --source-account "$ISSUER" \
