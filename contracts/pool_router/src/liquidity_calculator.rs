d3845bac5ae7da08d0437d1b0e6a46aeeea4014b
Joshua Blew <joshua@normalfinance.io>
2025-10-09 09:43:52 -0500

    fix: build fixes, temp remove sink

--- contracts/pool_router/src/liquidity_calculator.rs
+++ contracts/pool_router/src/liquidity_calculator.rs
@@ -1,5 +1,7 @@
 mod liquidity_calculator_client {
-    soroban_sdk::contractimport!(file = "../contracts/liquidity_calculator.wasm");
+    soroban_sdk::contractimport!(
+        file = "../../target/wasm32v1-none/release/liquidity_calculator.wasm"
+    );
 }
 
 pub use crate::liquidity_calculator::liquidity_calculator_client::Client as LiquidityCalculatorClient;
